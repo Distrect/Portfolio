72411926f59c4338e2bc6cef7c606e5453f7d58b
Distrect <sametsie34@gmail.com>
2024-08-01 11:27:38 +0300

    Almost finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { NavigationBar } from '../shared/component/navigation/navigation.component';
 import { AboutComponent, LandingComponent, ContactComponent } from '../section';
+import { FooterComponent } from '../shared/component/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['/src/style/main.style.scss', './app.component.scss'],
   standalone: true,
-  imports: [NavigationBar, LandingComponent, AboutComponent, ContactComponent],
+  imports: [
+    NavigationBar,
+    LandingComponent,
+    AboutComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
 })
 export class AppComponent implements AfterViewInit {
   title = 'portfolio';
